0e6f55f5eda98c75bec5096e5911c29678f66e75
Johann Gan <johanngan.us@gmail.com>
2017-07-01 18:25:15 -0500

    Removed spaces in arguments list in last line of readxml_test macro

--- TMVA/readxml/run_readxml_K_highpt_test.sh
+++ TMVA/readxml/run_readxml_K_highpt_test.sh
@@ -10,4 +10,4 @@ cat ../runTMVA_config.h > ${LOG_PATH}/config_K_highpt.log
 
 echo "Compiling..."
 root -l -b -q `echo 'compile_readxml.cpp("'${LIB_NAME}'")'`
-nohup unbuffer root -l -b -q `echo 'call_readxml.cpp("'${LIB_NAME}'", "config/backgroundTrees_K_highpt.xml", "_K_high")'` &> ${LOG_PATH}/readxml_K_highpt.log &
+nohup unbuffer root -l -b -q `echo 'call_readxml.cpp("'${LIB_NAME}'.so","config/backgroundTrees_K_highpt.xml","_K_high")'` &> ${LOG_PATH}/readxml_K_highpt.log &
