49e5a65f150dc26c1a9604c98522b185ca2785e9
Johann Gan <johanngan.us@gmail.com>
2017-07-02 03:42:43 -0500

    Changed run_myTMVA.sh to run the macro compiled.

--- TMVA/myTMVA_K_2_4/run_myTMVA.sh
+++ TMVA/myTMVA_K_2_4/run_myTMVA.sh
@@ -2,4 +2,4 @@
 
 config_file_path="/home/jg61/particle_reco/TMVA/myTMVA_K_2_4/config_K_2_4.xml"
 
-nohup unbuffer root -l -b -q `echo 'TMVAClassification.C("'${config_file_path}'")'` &> myTMVA.log &
+nohup unbuffer root -l -b -q `echo 'TMVAClassification.C+("'${config_file_path}'")'` &> myTMVA.log &
