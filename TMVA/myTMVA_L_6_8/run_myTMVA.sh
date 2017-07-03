adae42e5dbf8d6b8a7e2b25da889d3253c824d25
Johann Gan <johanngan.us@gmail.com>
2017-07-03 02:00:53 -0500

    Fixed the myTMVA run scripts

--- TMVA/myTMVA_L_6_8/run_myTMVA.sh
+++ TMVA/myTMVA_L_6_8/run_myTMVA.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-config_file_path="/home/jg61/particle_reco/TMVA/myTMVA_K_2_4/config_K_2_4.xml"
+config_file_path="/home/jg61/particle_reco/TMVA/myTMVA_L_6_8/config_L_6_8.xml"
 
 nohup unbuffer root -l -b -q `echo 'TMVAClassification.C+("'${config_file_path}'")'` &> myTMVA.log &
