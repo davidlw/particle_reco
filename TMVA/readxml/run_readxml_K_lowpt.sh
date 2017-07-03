55f3287bfc0c632e9dadeddaec90279a97607fc8
Johann Gan <johanngan.us@gmail.com>
2017-07-02 23:20:45 -0500

    More tweaking of run_readxml 6

--- TMVA/readxml/run_readxml_K_lowpt.sh
+++ TMVA/readxml/run_readxml_K_lowpt.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 READXML_PATH="/home/jg61/particle_reco/TMVA/readxml"
-LOG_PATH="/home/jg61/particle_reco/TMVA/readxml/logs"
+LOG_PATH="/home/jg61/particle_reco/TMVA/readxml/logs/K_2_4"
 CONFIG_FILE="/home/jg61/particle_reco/TMVA/myTMVA/config.xml"
-OUT_TAG="_K_2_4"
+OUT_TAG="_K"
 
 # SERVER_LIST and INDICES are the same size
 SERVER_LIST=(01 02 03 04 05 06 07)
@@ -28,7 +28,7 @@ ssh bonner${SERVER_LIST[$i]} "
     echo 'Executing...';
     for j in ${INDICES[$i]}; do
         echo hello
-        echo \`echo 'readxml.cc+(\"${CONFIG_FILE}\", \"${READXML_PATH}/IOconfig/split_K/backgroundTrees_K_\${j}.xml\", \"${OUT_TAG}_\${j}\")'\` &> ${LOG_PATH}/readxml_K_lowpt_\${j}.log
+        echo \"\echo 'readxml.cc+(\"${CONFIG_FILE}\", \"${READXML_PATH}/IOconfig/split_K/backgroundTrees_K_\${j}.xml\", \"${OUT_TAG}_\${j}\")' &> ${LOG_PATH}/readxml_K_lowpt_\${j}.log\"
         nohup unbuffer root -l -b -q \`echo 'readxml.cc+(\"${CONFIG_FILE}\", \"${READXML_PATH}/IOconfig/split_K/backgroundTrees_K_\${j}.xml\", \"${OUT_TAG}_\${j}\")'\` &> ${LOG_PATH}/readxml_K_lowpt_\${j}.log &
     done;"
 
