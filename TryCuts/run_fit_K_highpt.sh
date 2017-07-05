dceefa0b358673c14da4b99e46433fb36b1e48f1
Johann Gan <johanngan.us@gmail.com>
2017-07-05 12:19:52 -0500

    Added run_fit script for TryCuts_fit

--- TryCuts/run_fit_K_highpt.sh
+++ TryCuts/run_fit_K_highpt.sh
@@ -2,7 +2,7 @@
 
 FIT_EXEC="/home/jg61/particle_reco/TryCuts/src/TryCuts_fit"
 LOG_PATH="/home/jg61/particle_reco/TryCuts/logs"
-CONFIG_FILE="/home/jg61/particle_reco/TryCuts/config/TryCutsCfg_K_highpt.xml"
+CONFIG_FILE="/home/jg61/particle_reco/TryCuts/config/TryCutsCfg_K_highpt_full.xml"
 
 source start.sh
 
