d94c6bdbd07ab22762e9f627d5be14673a7493ab
Johann Gan <johanngan.us@gmail.com>
2017-07-05 13:14:31 -0500

    Added OUT_TAG macro to run_fit.sh

--- TryCuts/run_fit.sh
+++ TryCuts/run_fit.sh
@@ -3,7 +3,8 @@
 FIT_EXEC="/home/jg61/particle_reco/TryCuts/src/TryCuts_fit"
 LOG_PATH="/home/jg61/particle_reco/TryCuts/logs"
 CONFIG_FILE="/home/jg61/particle_reco/TryCuts/config/TryCutsCfg_K_highpt_full.xml"
+OUT_TAG=""
 
 source start.sh
 
-nohup unbuffer ${FIT_EXEC} ${CONFIG_FILE} &> ${LOG_PATH}/fits.log &
+nohup unbuffer ${FIT_EXEC} ${CONFIG_FILE} ${OUT_TAG} &> ${LOG_PATH}/fits.log &
