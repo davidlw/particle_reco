81f809044016b6cff2a2d26c6ac3c0f8effc236f
Johann Gan <johanngan.us@gmail.com>
2017-07-13 11:09:23 -0500

    Changed run_readxml_L_highpt for testing

--- TMVA/readxml/run_readxml_L_highpt.sh
+++ TMVA/readxml/run_readxml_L_highpt.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-PTBIN="L_6_8"
+PTBIN="L_10up"
 READXML_PATH="/home/jg61/particle_reco/TMVA/readxml"
 LOG_PATH="/home/jg61/particle_reco/TMVA/readxml/logs/${PTBIN}"
 CONFIG_FILE="/home/jg61/particle_reco/TMVA/myTMVA_${PTBIN}/config_${PTBIN}.xml"
 OUT_TAG="_${PTBIN}"
+EXEC="src/readxml"
 
-source start.sh
 cd ${READXML_PATH}
 
-nohup unbuffer root -l -b -q `echo 'readxml.cc+("'${CONFIG_FILE}'","/home/jg61/particle_reco/InputChain/config/dataTrees_L_highpt.xml","'${OUT_TAG}'")'` &> ${LOG_PATH}/readxml${OUT_TAG}.log &
+nohup unbuffer ${EXEC} ${CONFIG_FILE} /home/jg61/particle_reco/InputChain/config/dataTrees_L_highpt.xml ${OUT_TAG} &> ${LOG_PATH}/readxml${OUT_TAG}.log &
