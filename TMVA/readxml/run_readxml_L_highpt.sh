edd4ffd167790bd8914fa099d9146a62054511f0
Johann Gan <johanngan.us@gmail.com>
2017-07-13 12:12:53 -0500

    Took away output tag in run_readxml

--- TMVA/readxml/run_readxml_L_highpt.sh
+++ TMVA/readxml/run_readxml_L_highpt.sh
@@ -4,9 +4,9 @@ PTBIN="L_10up"
 READXML_PATH="/home/jg61/particle_reco/TMVA/readxml"
 LOG_PATH="/home/jg61/particle_reco/TMVA/readxml/logs/${PTBIN}"
 CONFIG_FILE="/home/jg61/particle_reco/TMVA/config/config_${PTBIN}.xml"
-OUT_TAG="_${PTBIN}"
+OUT_TAG=""
 EXEC="src/readxml"
 
 cd ${READXML_PATH}
 
-nohup unbuffer ${EXEC} ${CONFIG_FILE} /home/jg61/particle_reco/InputChain/config/dataTrees_L_highpt.xml ${OUT_TAG} &> ${LOG_PATH}/readxml${OUT_TAG}.log &
+nohup unbuffer ${EXEC} ${CONFIG_FILE} /home/jg61/particle_reco/InputChain/config/dataTrees_L_highpt.xml ${OUT_TAG} &> ${LOG_PATH}/readxml_${PTBIN}.log &
