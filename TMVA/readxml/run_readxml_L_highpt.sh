fd3e7fd81db253a55c25e2f9e79b1f3cfd31a16f
Johann Gan <johanngan.us@gmail.com>
2017-07-13 11:21:05 -0500

    Fixed run_readxml_L_highpt to use the right config file

--- TMVA/readxml/run_readxml_L_highpt.sh
+++ TMVA/readxml/run_readxml_L_highpt.sh
@@ -3,7 +3,7 @@
 PTBIN="L_10up"
 READXML_PATH="/home/jg61/particle_reco/TMVA/readxml"
 LOG_PATH="/home/jg61/particle_reco/TMVA/readxml/logs/${PTBIN}"
-CONFIG_FILE="/home/jg61/particle_reco/TMVA/myTMVA_${PTBIN}/config_${PTBIN}.xml"
+CONFIG_FILE="/home/jg61/particle_reco/TMVA/config/config_${PTBIN}.xml"
 OUT_TAG="_${PTBIN}"
 EXEC="src/readxml"
 
