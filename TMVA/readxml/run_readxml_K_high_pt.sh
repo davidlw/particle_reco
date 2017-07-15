a9ff8c0e694d294769191068e845c2c05c5d13e7
Johann Gan <johanngan.us@gmail.com>
2017-07-15 17:06:52 -0500

    Modified InputChain config files to work with new skims

---
+++ TMVA/readxml/run_readxml_K_high_pt.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+PTBIN="K_6_8"
+READXML_PATH="/home/jg61/particle_reco/TMVA/readxml"
+LOG_PATH="/home/jg61/particle_reco/TMVA/readxml/logs/${PTBIN}"
+CONFIG_FILE="/home/jg61/particle_reco/TMVA/config/config_${PTBIN}.xml"
+EXEC="/home/jg61/particle_reco/TMVA/readxml/src/readxml"
+
+source start.sh
+cd ${READXML_PATH}
+
+for i in pPb Pbp; do
+    nohup unbuffer EXEC ${CONFIG_FILE} "/home/jg61/particle_reco/InputChain/config/K_${i}_high_pt.xml" _${PT_BIN} &> ${LOG_PATH}/readxml_${PT_BIN}.log &
+done
---
+++ TMVA/readxml/run_readxml_L_4_6.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+PTBIN="L_4_6"
+READXML_PATH="/home/jg61/particle_reco/TMVA/readxml"
+LOG_PATH="/home/jg61/particle_reco/TMVA/readxml/logs/${PTBIN}"
+CONFIG_FILE="/home/jg61/particle_reco/TMVA/config/config_${PTBIN}.xml"
+EXEC="/home/jg61/particle_reco/TMVA/readxml/src/readxml"
+
+source start.sh
+cd ${READXML_PATH}
+
+for i in pPb Pbp; do
+    nohup unbuffer EXEC ${CONFIG_FILE} "/home/jg61/particle_reco/InputChain/config/L_${i}_4_6.xml" _${PT_BIN} &> ${LOG_PATH}/readxml_${PT_BIN}.log &
+done
