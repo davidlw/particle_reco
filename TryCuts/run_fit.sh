05d3356bdadb7c36e276a0176c516c4438b3b01d
Johann Gan <johanngan.us@gmail.com>
2017-07-20 16:26:06 -0500

    Made run_fit.sh for TryCuts more configurable with CFG_TAG and PTBIN

--- TryCuts/run_fit.sh
+++ TryCuts/run_fit.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+PTBIN="L_10up"
+CFG_TAG="md1"
 FIT_EXEC="/home/jg61/particle_reco/TryCuts/src/TryCuts_fit"
-LOG_PATH="/home/jg61/particle_reco/TryCuts/logs"
-CONFIG_FILE="/home/jg61/particle_reco/TryCuts/config/TryCutsCfg_K_highpt_full.xml"
-OUT_TAG=""
+LOG_PATH="/home/jg61/particle_reco/TryCuts/logs/${PTBIN}"
+CONFIG_FILE="/home/jg61/particle_reco/TryCuts/config/${CFG_TAG}/TryCutsCfg_${PTBIN}.xml"
+
 
 source start.sh
 
-nohup unbuffer ${FIT_EXEC} ${CONFIG_FILE} ${OUT_TAG} &> ${LOG_PATH}/fits.log &
+nohup unbuffer ${FIT_EXEC} ${CONFIG_FILE} _${CFG_TAG} &> ${LOG_PATH}/fits.log &
