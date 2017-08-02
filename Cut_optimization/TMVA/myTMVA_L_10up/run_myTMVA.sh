0fec0827e5f9611d294ea682d69cbb5311240121
Johann Gan <johanngan.us@gmail.com>
2017-08-02 15:31:51 -0500

    Re-removed old stuff that came back in after copying to the Cut_optimization directory

--- Cut_optimization/TMVA/myTMVA_L_10up/run_myTMVA.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-config_file_path="/home/jg61/particle_reco/TMVA/myTMVA_L_10up/config_L_10up.xml"
-
-nohup unbuffer root -l -b -q `echo 'TMVAClassification.C+("'${config_file_path}'")'` &> myTMVA.log &
--- Cut_optimization/TMVA/myTMVA_L_4_6/run_myTMVA.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-config_file_path="/home/jg61/particle_reco/TMVA/myTMVA_L_4_6/config_L_4_6.xml"
-
-nohup unbuffer root -l -b -q `echo 'TMVAClassification.C+("'${config_file_path}'")'` &> myTMVA.log &
