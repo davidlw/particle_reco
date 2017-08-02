676f402ee2f6c41e8583b9d42fdc00ee1e9754bc
Johann Gan <johanngan.us@gmail.com>
2017-08-02 14:10:58 -0500

    Removed old myTMVA stuff

--- TMVA/myTMVA_L_6_8/run_myTMVA.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-config_file_path="/home/jg61/particle_reco/TMVA/myTMVA_L_6_8/config_L_6_8.xml"
-
-nohup unbuffer root -l -b -q `echo 'TMVAClassification.C+("'${config_file_path}'")'` &> myTMVA.log &
--- TMVA/myTMVA_L_8_10/run_myTMVA.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-config_file_path="/home/jg61/particle_reco/TMVA/myTMVA_L_8_10/config_L_8_10.xml"
-
-nohup unbuffer root -l -b -q `echo 'TMVAClassification.C+("'${config_file_path}'")'` &> myTMVA.log &
