220886cf354ef842d8d1f4ad724e595ecf005549
Johann Gan <johanngan.us@gmail.com>
2017-08-02 15:29:47 -0500

    Deleted optimization stuff outside of the optimization directory

--- TMVA/myTMVA/run_myTMVA.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-PTBIN="L_10up"
-CONFIG_FILE_PATH="../config/config_${PTBIN}.xml"
-LOG_PATH="logs"
-EXEC="src/TMVAClassification"
-
-nohup unbuffer ${EXEC} ${CONFIG_FILE_PATH} &> ${LOG_PATH}/${PTBIN}.log &
